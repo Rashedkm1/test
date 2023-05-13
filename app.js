68fd249af6d5e2ed12362df982f4d58a92b5bfb6
Rashed Khan Menon <128370660+rashedkm1@users.noreply.github.com>
2023-05-13 17:31:45 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+let pass = [];
+
+let obj = {
+    name : "Rashed",
+    age: 30
+}
+
+pass.push(obj);
--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-1
+const people = [
+    { 
+        id: 1,
+        name: "product 1",
+        price: 10
+    },
+    { 
+        id: 2,
+        name: "product 2",
+        price: 10
+    },
+    { 
+        id: 3,
+        name: "product 3",
+        price: 10
+    },
+
+  ];
+//   const person = people.find(person => person.age > 30);
+//   console.log(person); // Output: { name: "Bob", age: 40 }
+
+  const person = people.find(person => person.id === -1);
+  const index = people.findIndex(person => person.id === 10);
+  console.log(index); // Output: 2
+  console.log(person); // Output: 2
+  
